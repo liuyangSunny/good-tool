3bb150ffef637183ea1886f058d4ffd611d01409
liuyang <liuyang_deve@sina.com>
2019-10-29 18:05:41 +0800

    mod LocalDateTimeUtil

--- good-tool-core/src/main/java/com/github/liuyangSunny/good/tool/core/time/LocalDateTimeUtil.java
+++ good-tool-core/src/main/java/com/github/liuyangSunny/good/tool/core/time/LocalDateTimeUtil.java
@@ -1,20 +1,54 @@
 package com.github.liuyangSunny.good.tool.core.time;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
 /**
  * LocalDateTime工具类
+ *
  * @author liuyang
  */
 public class LocalDateTimeUtil {
 
-    public LocalDateTime toLocalDateTime(Date date) {
-        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    /**
+     * 将Date转为LocalDateTime
+     *
+     * @param date Date对象
+     * @return LocalDateTime对象
+     */
+    public static LocalDateTime toLocalDateTime(Date date) {
+        return toLocalDateTime(date.toInstant());
     }
 
-    public Long toMillis(LocalDateTime localDateTime) {
-        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    /**
+     * LocalDateTime转 Instant
+     *
+     * @param dateTime 时间
+     * @return Instant
+     */
+    public static Instant toInstant(LocalDateTime dateTime) {
+        return dateTime.atZone(ZoneId.systemDefault()).toInstant();
+    }
+
+    /**
+     * Instant 转 LocalDateTime
+     *
+     * @param instant Instant
+     * @return Instant
+     */
+    public static LocalDateTime toLocalDateTime(Instant instant) {
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+    /**
+     * 获取LocalDateTime的毫秒数
+     *
+     * @param localDateTime LocalDateTime
+     * @return 毫秒数
+     */
+    public static Long toMillis(LocalDateTime localDateTime) {
+        return toInstant(localDateTime).toEpochMilli();
     }
 }
